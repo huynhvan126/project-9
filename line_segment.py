be273aaa9a1a6307701f7303c090a3096838f523
huynhvan126 <165982795+huynhvan126@users.noreply.github.com>
2024-05-29 23:12:39 -0700

    line_segment.py

--- line_segment.py
+++ line_segment.py
@@ -5,18 +5,21 @@
 class Point:
     """Represent a point in 2D space."""
     def __init__(self, x_coord, y_coord):
-        self.x_coord = x_coord
-        self.y_coord = y_coord
+        """Initialize the point object with the given x and y coordinates."""
+        self._x_coord = x_coord
+        self._y_coord = y_coord
     def get_x_coord(self):
         return self._x_coord
     def get_y_coord(self):
         return self._y_coord
-    def distance(self, other_point):
-        x1, y1 = self._x_coord, self._y_coord
-        x2, y2 = other_point.get_x_coord(), other_point.get_y_coord()
-        return ((x2-x1)**2 + (y2-y1)**2)**0.5
+    def distance_to(self, other_point):
+        """Calculate the distance between this point and the other."""
+        x_diff = self._x_coord - other_point.get_x_coord()
+        y_diff = self._y_coord - other_point.get_y_coord()
+        return (x_diff ** 2 + y_diff ** 2) ** 0.5
 class LineSegment:
     def __init__(self, endpoint_1, endpoint_2):
+        """Initialize the line segment object with the given 2 endpoints."""
         self._endpoint_1 = endpoint_1
         self._endpoint_2 = endpoint_2
     def get_endpoint_1(self):
@@ -26,12 +29,15 @@ class LineSegment:
     def length(self):
         return self._endpoint_1.distance(self._endpoint_2)
     def slope(self):
-        x1, y1 = self._endpoint_1.get_x_coord(), self._endpoint_1.get_y_coord()
-        x2, y2 = self._endpoint_2.get_x_coord(), self._endpoint_2.get_y_coord()
-        if x2 - x1 == 0:
-            return float ('int')
-        return (y2-y1)/(x2-x1)
-    def is_parallel(self, other_line):
+        x1 = self._endpoint_1().get_x_coord()
+        y1 = self._endpoint_1().get_y_coord()
+        x2 = self._endpoint_2().get_x_coord()
+        y2 = self._endpoint_2().get_y_coord()
+        if abs (x2 - x1) < 0.000001
+            return float('int')
+        else:
+            return (y2 - y1) / (x2 - x1)
+    def is_parallel_to(self, other_line):
         slope1 = self.slope()
         slope2 = other_line.slope()
         return abs(slope1-slope2) < 0.000001
